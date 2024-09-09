cf4fc7a2ef491864f7151e762c83c11b317c689c
Esteban Morales <esteballs@the-spic-machine.local>
2024-09-09 15:32:23 -0400

    new notes

---
+++ MTH 542/secondClass.r
@@ -0,0 +1,12 @@
+pnorm(1) '''standard normal CDF i.e. p(x) = (-infinity, x)
+ with normal dist m = 0, s =1''' 
+
+
+
+rnorm(10)
+''' random trial of n = 10, of a std normal dist.'
+z = rnorm(100) 
+'''length(z) gives length''
+
+z<- ]
+
